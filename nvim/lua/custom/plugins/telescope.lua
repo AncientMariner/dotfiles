278c6cfe3a3f54c698bf5868fbdbe29487f7f66c
AncientMariner <xander.lysak@gmail.com>
2025-04-23 18:11:32 +0200

    win conf update

--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -92,7 +92,7 @@ return {
             }
         })
 
-        telescope.load_extension("fzf")
+        -- telescope.load_extension("fzf")
         telescope.load_extension("file_browser")
         telescope.load_extension("live_grep_args")
 
