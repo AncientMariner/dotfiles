c8515770cf8d1e7b23fa6a449b18808c3fbacadf
AncientMariner <xander.lysak@gmail.com>
2026-04-02 12:04:34 +0200

    treesitter fix

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -4,50 +4,56 @@ vim.pack.add({
 	"https://github.com/nvim-treesitter/nvim-treesitter-context",
 })
 
--- Treesitter context setup
-require("treesitter-context").setup({
-	enable = true,
-	mode = "topline",
-	line_numbers = true
-})
+-- Setup treesitter after plugins are loaded
+vim.schedule(function()
+	-- Treesitter context setup
+	local ok_context, treesitter_context = pcall(require, "treesitter-context")
+	if ok_context then
+		treesitter_context.setup({
+			enable = true,
+			mode = "topline",
+			line_numbers = true
+		})
+	end
 
--- Treesitter setup
--- local treesitter = require("nvim-treesitter.configs")
-local treesitter = require("nvim-treesitter")
-
-treesitter.setup({
-	ensure_installed = {
-		"csv", "dockerfile", "gitignore", "go", "gomod", "gosum",
-		"gowork", "javascript", "json", "lua", "markdown", 
-		-- "php",
-		-- "proto", 
-		-- "python", 
-		-- "rego", 
-		-- "ruby", 
-		"sql", 
-		-- "svelte", 
-		"yaml", 
-		-- "just", 
-		"vimdoc", "bash" 
-	},
-	indent = {enable = true},
-	auto_install = true,
-	sync_install = false,
-	highlight = {
-		enable = true,
-		disable = {"csv"}, -- preferring chrisbra/csv.vim
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "<c-space>",
-			node_incremental = "<c-space>",
-			scope_incremental = "<c-s>",
-			node_decremental = "<c-backspace>",
-		},
-	},
-	textobjects = {select = {enable = true, lookahead = true}}
-})
+	-- Treesitter setup
+	local ok_ts, treesitter = pcall(require, "nvim-treesitter.configs")
+	if ok_ts then
+		treesitter.setup({
+			ensure_installed = {
+				"csv", "dockerfile", "gitignore", "go", "gomod", "gosum",
+				"gowork", "javascript", "json", "lua", "markdown", 
+				-- "php",
+				-- "proto", 
+				-- "python", 
+				-- "rego", 
+				-- "ruby", 
+				"sql", 
+				-- "svelte", 
+				"yaml", 
+				-- "just", 
+				"vimdoc", "bash" 
+			},
+			indent = {enable = true},
+			auto_install = true,
+			sync_install = false,
+			highlight = {
+				enable = true,
+				disable = {"csv"}, -- preferring chrisbra/csv.vim
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<c-space>",
+					node_incremental = "<c-space>",
+					scope_incremental = "<c-s>",
+					node_decremental = "<c-backspace>",
+				},
+			},
+			textobjects = {select = {enable = true, lookahead = true}}
+		})
+	end
+end)
 
 vim.api.nvim_create_autocmd("PackChanged", {
 	desc = "Handle nvim-treesitter updates",
