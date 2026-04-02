67157f4f104c664375196bc2726823181a6244a3
AncientMariner <xander.lysak@gmail.com>
2026-04-02 14:31:59 +0200

    try different approach

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,230 @@
+vim.pack.add({
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/stevearc/conform.nvim",
+	"https://github.com/williamboman/mason.nvim",
+	"https://github.com/williamboman/mason-lspconfig.nvim",
+	"https://github.com/hrsh7th/cmp-nvim-lsp",
+	"https://github.com/hrsh7th/cmp-buffer",
+	"https://github.com/hrsh7th/cmp-path",
+	"https://github.com/hrsh7th/cmp-cmdline",
+	"https://github.com/hrsh7th/nvim-cmp",
+	"https://github.com/L3MON4D3/LuaSnip",
+	"https://github.com/saadparwaiz1/cmp_luasnip",
+	"https://github.com/j-hui/fidget.nvim",
+})
+
+-- Setup conform for formatting
+require("conform").setup({
+	formatters_by_ft = {
+	}
+})
+
+local cmp = require('cmp')
+local cmp_lsp = require("cmp_nvim_lsp")
+local capabilities = vim.tbl_deep_extend(
+	"force",
+	{},
+	vim.lsp.protocol.make_client_capabilities(),
+	cmp_lsp.default_capabilities())
+
+require("fidget").setup({})
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		-- "rust_analyzer",
+		"gopls",
+		-- "vtsls",
+		"tailwindcss",
+	},
+	handlers = {
+		function(server_name) -- default handler (optional)
+			require("lspconfig")[server_name].setup {
+				capabilities = capabilities
+			}
+		end,
+
+		zls = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.zls.setup({
+				root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+				settings = {
+					zls = {
+						enable_inlay_hints = true,
+						enable_snippets = true,
+						warn_style = true,
+					},
+				},
+			})
+			vim.g.zig_fmt_parse_errors = 0
+			vim.g.zig_fmt_autosave = 0
+
+		end,
+		["lua_ls"] = function()
+			local lspconfig = require("lspconfig")
+
+			lspconfig.lua_ls.setup {
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = {
+							version = 'LuaJIT',
+						},
+						diagnostics = {
+							globals = { 'vim' },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						format = {
+							enable = true,
+							-- Put format options here
+							-- NOTE: the value should be STRING!!
+							defaultConfig = {
+								indent_style = "space",
+								indent_size = "2",
+							}
+						},
+					}
+				}
+			}
+		end,
+		["tailwindcss"] = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+				filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "svelte", "heex" },
+			})
+		end,
+	}
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+	callback = function(event)
+		local map = function(keys, func, desc, mode)
+			mode = mode or 'n'
+			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+		end
+		-- Jump to the definition of the word under your cursor.
+		--  This is where a variable was first declared, or where a function is defined, etc.
+		--  To jump back, press <C-t>.
+		map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+		-- Find references for the word under your cursor.
+		map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+		-- Jump to the implementation of the word under your cursor.
+		--  Useful when your language has ways of declaring types without an actual implementation.
+		map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+		
+		-- Jump to the type of the word under your cursor.
+		--  Useful when you're not sure what type a variable is and you want to see
+		--  the definition of its *type*, not where it was *defined*.
+		map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+
+		-- Fuzzy find all the symbols in your current document.
+		--  Symbols are things like variables, functions, types, etc.
+		map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
+		-- Fuzzy find all the symbols in your current workspace.
+		--  Similar to document symbols, except searches over your entire project.
+		map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+		-- Rename the variable under your cursor.
+		--  Most Language Servers support renaming across files, etc.
+		map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+		-- Execute a code action, usually your cursor needs to be on top of an error
+		-- or a suggestion from your LSP for this to activate.
+		-- using inline one, uncomment if want to use this
+		-- map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+
+		-- WARN: This is not Goto Definition, this is Goto Declaration.
+		--  For example, in C this would take you to the header.
+		map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+		-- The following two autocommands are used to highlight references of the
+		-- word under your cursor when your cursor rests there for a little while.
+		--    See `:help CursorHold` for information about when this is executed
+		--
+		-- When you move your cursor, the highlights will be cleared (the second autocommand).
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+		if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+			local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+			vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+				buffer = event.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.document_highlight,
+			})
+
+			vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+				buffer = event.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.clear_references,
+			})
+
+			vim.api.nvim_create_autocmd('LspDetach', {
+				group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+				callback = function(event2)
+					vim.lsp.buf.clear_references()
+					vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+				end,
+			})
+		end
+
+		-- The following code creates a keymap to toggle inlay hints in your
+		-- code, if the language server you are using supports them
+		--
+		-- This may be unwanted, since they displace some of your code
+		if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+			map('<leader>th', function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+			end, '[T]oggle Inlay [H]ints')
+		end
+
+	end,
+})
+
+-- LSP servers and clients are able to communicate to each other what features they support.
+--  By default, Neovim doesn't support everything that is in the LSP specifination.
+--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+local capabilities_extended = vim.lsp.protocol.make_client_capabilities()
+capabilities_extended = vim.tbl_deep_extend('force', capabilities_extended, require('cmp_nvim_lsp').default_capabilities())
+
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+		['<C-y>'] = cmp.mapping.confirm({ select = true }),
+		["<C-Space>"] = cmp.mapping.complete(),
+	}),
+	sources = cmp.config.sources({
+		{ name = "copilot", group_index = 2 },
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' }, -- For luasnip users.
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+vim.diagnostic.config({
+	-- update_in_insert = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+})
